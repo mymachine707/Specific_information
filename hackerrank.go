f7a15ae76464df96ab156593fed67a0e65b033cd
mymachine707 <109611597+mymachine707@users.noreply.github.com>
2022-11-15 18:32:36 +0500

    Create hackerrank.go
    Hackerrank misol

---
+++ hackerrank.go
@@ -0,0 +1,7 @@
+//https://www.hackerrank.com/challenges/kangaroo/problem?isFullScreen=true
+
+ if (x1 + v1 == x2 + v2) || ( x1<x2 && v1>v2) &&
+    ((x2 + v2)%(x1 + v1) == 0 || (x2 - x1)%(v1 - v2) == 0){
+        return "YES"
+    } 
+ return "NO"
