87559832b6bd6ecd9bd2fc0ef1477daf0c7af825
mymachine707 <109611597+mymachine707@users.noreply.github.com>
2022-11-15 21:05:10 +0500

    Update hackerrank.go

--- hackerrank.go
+++ hackerrank.go
@@ -1,5 +1,5 @@
 //https://www.hackerrank.com/challenges/kangaroo/problem?isFullScreen=true
-
+// Farhod aka javobi
  if (x1 + v1 == x2 + v2) || ( x1<x2 && v1>v2) &&
     ((x2 + v2)%(x1 + v1) == 0 || (x2 - x1)%(v1 - v2) == 0){
         return "YES"
@@ -7,7 +7,7 @@
  return "NO"
 
 
-// Abdulahad Javobi
+// Abdulahad aka javobi Javobi
 
 if v1>v2 && (x2-x1)%(v1-v2)==0{
         return "YES"
