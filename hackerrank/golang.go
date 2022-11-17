b04012f4045f12ecd9d3535c59741ccd10058d72
mymachine707 <109611597+mymachine707@users.noreply.github.com>
2022-11-17 14:16:25 +0500

    change

--- hackerrank/golang.go
+++ hackerrank/golang.go
@@ -13,3 +13,23 @@ if v1>v2 && (x2-x1)%(v1-v2)==0{
         return "YES"
     }
     return "NO"
+
+//https://www.hackerrank.com/challenges/divisible-sum-pairs/problem?isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+
+func divisibleSumPairs(n int32, k int32, ar []int32) int32 {
+    // Write your code here
+    var ans int32
+    
+    for i:=0;i<len(ar);i++{
+        for d:=0;d<len(ar);d++{
+            if i<d && (ar[i]+ar[d])%k==0 {
+                ans++
+            }
+        }
+    }
+    
+    
+    return ans
+}
+
+
